a65f6599b8a5bd531741b8b7968eebc76bfd128c
Hong, Yoonho <nrhong@gmail.com>
2023-02-15 12:01:07 +0900

    add init.R

---
+++ init.R
@@ -0,0 +1,11 @@
+
+my_packages = c("DT")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, clean=TRUE, quiet=TRUE)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
